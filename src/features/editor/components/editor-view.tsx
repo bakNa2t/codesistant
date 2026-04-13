aed51ae97162c7afa96236686d3e0171146cfe1a
bakNa2t <ok2.lock@gmail.com>
2026-04-13 07:47:30 +0300

    Add tab's breadcrumbs

--- src/features/editor/components/editor-view.tsx
+++ src/features/editor/components/editor-view.tsx
@@ -1,13 +1,19 @@
 import { TopNavigation } from "./top-navigation";
 
+import { FileBreadcrumbs } from "./file-breadcrumbs";
+
 import { Id } from "../../../../convex/_generated/dataModel";
+import { useEditor } from "../hooks/use-editor";
 
 export const EditorView = ({ projectId }: { projectId: Id<"projects"> }) => {
+  const { activeTabId } = useEditor(projectId);
+
   return (
     <div className="flex flex-col h-full">
       <div className="flex items-center">
         <TopNavigation projectId={projectId} />
       </div>
+      {activeTabId && <FileBreadcrumbs projectId={projectId} />}
     </div>
   );
 };
