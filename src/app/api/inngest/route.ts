5e03da2d6b3be238224300b930e7e76c11981e0b
bakNa2t <ok2.lock@gmail.com>
2026-06-22 07:17:15 +0300

    Finished inngest export to github

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,10 +1,12 @@
 import { serve } from "inngest/next";
+
 import { inngest } from "@/inngest/client";
-import { demoError, demoGenerate } from "@/inngest/functions";
 import { processMessage } from "@/features/conversations/inngest/process-message";
+import { exportToGithub } from "@/features/projects/inngest/export-to-github";
+import { importGithubRepo } from "@/features/projects/inngest/import-github-repo";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [demoGenerate, demoError, processMessage],
+  functions: [processMessage, importGithubRepo, exportToGithub],
 });
